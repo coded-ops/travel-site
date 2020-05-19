3c51abf2004a6dee59c844db1bb991d1072a0f51
Josh_Adin <joshua.adinfono@gmail.com>
2020-05-19 00:11:54 -0400

    complete desktop and mobile header styles

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,7 @@
 import '../styles/styles.css';
+import MobileMenu from './modules/MobileMenu';
+
+let mobileMenu = new MobileMenu();
 
 // accept the hot updates if it makes sense to accept them
 if (module.hot) {
