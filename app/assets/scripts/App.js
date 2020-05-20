af43b4dd7865f7b9b2469d966fc76583c93e4037
Josh_Adin <joshua.adinfono@gmail.com>
2020-05-19 23:33:28 -0400

    completed the reveal on scroll behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,11 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
-let mobileMenu = new MobileMenu();
+new MobileMenu();
+
+new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
+new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
 
 // accept the hot updates if it makes sense to accept them
 if (module.hot) {
