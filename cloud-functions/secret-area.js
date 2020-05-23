945dda4a4aa8bec97fec5bbb1e7cacbad4987274
Josh_Adin <joshua.adinfono@gmail.com>
2020-05-23 18:31:44 -0400

    updated cloud function with temp CORS solution

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,5 +1,19 @@
 exports.handler = function(event, context, callback) {
 
+    // CORS solution to test locally. Can also use Netlify dev
+    const headers = {
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Headers' : 'Content-Type'
+    }
+
+    if (event.httpMethod !== 'POST') {
+        return callback(null, {
+            statusCode: 200,
+            body: 'This was not a POST request',
+            headers,
+        })
+    }
+
     const secretContent = `
         <h3>Welcome To The Secret Area</h3>
         <p>Here we can tell you that the sky is <strong>blue</>, and two plus two equals four.</p>
@@ -17,10 +31,12 @@ exports.handler = function(event, context, callback) {
         callback(null, {
             statusCode: 200,
             body: secretContent,
+            headers,
         });
     } else {
         callback(null, {
             statusCode: 401,
+            headers,
         });
     }
 
